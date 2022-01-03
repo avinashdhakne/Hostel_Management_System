7fad0b22e438ae337fc7ddcfec73783f75a2388b
Avinash <asdhakne21@gmail.com>
2022-01-03 21:12:26 +0530

    Initial commit

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+
+/**
+ *
+ * @author asdha
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/vit","root","root");
+            return con;
+        }
+        catch(Exception e){
+            System.out.println("Exception: "+e);
+            return null;
+        }
+    }
+    
+}
